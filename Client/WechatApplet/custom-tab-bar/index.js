0e06a446fd2b282b7845446224f3db3d4e6805a3
ZShijun <35375949+ZShijun@users.noreply.github.com>
2022-05-22 00:09:43 +0800

    微信小程序添加导航栏

---
+++ Client/WechatApplet/custom-tab-bar/index.js
@@ -0,0 +1,30 @@
+Component({
+  data: {
+    selected: 0,
+    color: "#707070",
+    selectedColor: "#fcc21b",
+    list: [{
+      pagePath: "/pages/index-valuation/index",
+      iconPath: "/images/icon_index_valuation_n.png",
+      selectedIconPath: "/images/icon_index_valuation_y.png",
+      text: "指数估值"
+    }, {
+      pagePath: "/pages/market-sentiment/index",
+      iconPath: "/images/icon_market_sentiment_n.png",
+      selectedIconPath: "/images/icon_market_sentiment_y.png",
+      text: "市场情绪"
+    }]
+  },
+  attached() {
+  },
+  methods: {
+    switchTab(e) {
+      const data = e.currentTarget.dataset
+      const url = data.path
+      wx.switchTab({url})
+      this.setData({
+        selected: data.index
+      })
+    }
+  }
+})
